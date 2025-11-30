5d0a6184865ba0bf6bebb3b48f7a558e6f638f3d
José Eudásio de Monte Viana <jeaprender@gmail.com>
2025-11-30 09:33:50 -0300

    Update responsavel.py

--- responsavel.py
+++ responsavel.py
@@ -1,5 +1,9 @@
-from src.usuario import Usuario
+from usuario import Usuario
 
 class Responsavel(Usuario):
-    def __init__(self, id_usuario, nome, email, senha, telefone):
-        super().__init__(id_usuario, nome, email, senha, telefone=telefone)
+    def __init__(self, id_usuario, nome, email, senha, cpf, telefone):
+        super().__init__(id_usuario, nome, email, senha, cpf, telefone)
+
+    def gerar_relatorio(self, relatorio):
+        print(f"Responsável {self.nome} gerou o relatório {relatorio.nome_relatorio}")
+
