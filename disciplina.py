03b112475c4f5878782d336d716c311f005188f1
José Eudásio de Monte Viana <jeaprender@gmail.com>
2025-11-30 09:35:47 -0300

    Update disciplina.py

--- disciplina.py
+++ disciplina.py
@@ -1,12 +1,5 @@
 class Disciplina:
-    def __init__(self, id_disciplina, nome):
+    def __init__(self, id_disciplina, nome_disciplina):
         self.id_disciplina = id_disciplina
-        self.nome = nome
-        self.notas = []
-        self.frequencias = []
+        self.nome_disciplina = nome_disciplina
 
-    def adicionar_nota(self, nota):
-        self.notas.append(nota)
-
-    def adicionar_frequencia(self, freq):
-        self.frequencias.append(freq)
