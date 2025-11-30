8bc0fa47d926038496a0b6c7eebe489d2b18f736
José Eudásio de Monte Viana <jeaprender@gmail.com>
2025-11-30 09:31:33 -0300

    Update professor.py

--- professor.py
+++ professor.py
@@ -1,27 +1,12 @@
-from src.usuario import Usuario
-from src.nota import Nota
-from src.frequencia import Frequencia
-from datetime import date
+from usuario import Usuario
 
 class Professor(Usuario):
-    def __init__(self, id_usuario, nome, email, senha, data_nascimento):
-        super().__init__(id_usuario, nome, email, senha)
-        self.data_nascimento = data_nascimento
-        self.notas = []
-        self.frequencias = []
+    def __init__(self, id_usuario, nome, email, senha, cpf, telefone):
+        super().__init__(id_usuario, nome, email, senha, cpf, telefone)
 
-    def lancar_nota(self, id_nota, aluno, disciplina, valor, tipo_avaliacao):
-        nota = Nota(id_nota, aluno, disciplina, valor, tipo_avaliacao)
-        self.notas.append(nota)
-        aluno.adicionar_nota(nota)
-        disciplina.adicionar_nota(nota)
-        print(f"Nota {valor} lançada para {aluno.nome} em {disciplina.nome}.")
-        return nota
+    def lancar_nota(self, nota):
+        print(f"Professor {self.nome} lançou a nota {nota.valor} para o aluno {nota.aluno.nome}")
 
-    def registrar_frequencia(self, id_frequencia, aluno, disciplina, status):
-        freq = Frequencia(id_frequencia, aluno, disciplina, date.today(), status)
-        self.frequencias.append(freq)
-        aluno.adicionar_frequencia(freq)
-        disciplina.adicionar_frequencia(freq)
-        print(f"Frequência registrada: {aluno.nome} - {status} ({disciplina.nome})")
-        return freq
+    def registrar_frequencia(self, frequencia):
+        print(f"Professor {self.nome} registrou frequência de {frequencia.status} para o aluno {frequencia.aluno.nome}")
+        
