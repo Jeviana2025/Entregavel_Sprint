0fde8220b459349f1fd7e6a2af80a89f8bf1be74
José Eudásio de Monte Viana <jeaprender@gmail.com>
2025-11-30 09:30:16 -0300

    Update usuario.py

--- usuario.py
+++ usuario.py
@@ -1,24 +1,25 @@
 class Usuario:
-    def __init__(self, id_usuario, nome, email, senha, cpf=None, telefone=None, endereco=None):
+    def __init__(self, id_usuario, nome, email, senha, cpf, telefone):
         self.id_usuario = id_usuario
         self.nome = nome
         self.email = email
         self.senha = senha
         self.cpf = cpf
         self.telefone = telefone
-        self.endereco = endereco
 
     def fazer_login(self, email, senha):
         return self.email == email and self.senha == senha
 
     def criar(self):
-        print(f"Usuário {self.nome} criado.")
+        print(f"Usuário {self.nome} criado no sistema.")
 
     def ler(self):
-        print(f"Usuário: {self.nome}, Email: {self.email}")
+        return vars(self)
 
-    def atualizar(self):
-        print(f"Usuário {self.nome} atualizado.")
+    def atualizar(self, **kwargs):
+        for chave, valor in kwargs.items():
+            setattr(self, chave, valor)
 
     def excluir(self):
-        print(f"Usuário {self.nome} excluído.")
+        print(f"Usuário {self.nome} removido do sistema.")
+
